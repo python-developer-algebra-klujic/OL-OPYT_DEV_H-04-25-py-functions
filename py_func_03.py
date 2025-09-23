86866e8960746f32cca788423a49d0fc233e146f
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-23 21:08:34 +0200

    Create py_func_03.py

---
+++ py_func_03.py
@@ -0,0 +1,35 @@
+import typing
+
+
+def podijeli(a:int, b:int) -> None:
+    print(a / b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+x = 4
+y = 5
+
+podijeli(5, 15)
+podijeli(b=15, a=5)
+podijeli(15, 5)
+podijeli(x, y)
+podijeli(b=y, a=x)
+podijeli(y, x)
