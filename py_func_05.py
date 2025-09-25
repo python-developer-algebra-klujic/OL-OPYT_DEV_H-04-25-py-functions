0e1daf6493f9ec1a5dd2c6f71124bc45bc4ba581
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-25 18:48:02 +0200

    Update py_func_05.py

--- py_func_05.py
+++ py_func_05.py
@@ -4,10 +4,16 @@
 def generate_full_name(first_name:str, last_name:str = '') -> str:
     # full_name = f'{first_name} {last_name}'
     # return full_name
-    return f'{first_name}_{last_name}'
+    if last_name == '':
+        return f'{first_name}'
+    else:
+        return f'{first_name} {last_name}'
 
 
 
+def podijeli(x, y):
+    rezultat = x / y
+    return rezultat
 
 
 
@@ -35,3 +41,5 @@ first_name = 'Ana'
 # last_name = 'Anic'
 full_name = generate_full_name(first_name)
 print(full_name)
+
+print( podijeli(5, 0)  )
